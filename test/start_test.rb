0107de116e6063855e9879123c6615e67cafa6f1
Ross Perry <perryr16@gmail.com>
2020-03-22 15:38:43 -0600

    finish testing start methods

--- test/start_test.rb
+++ test/start_test.rb
@@ -25,5 +25,45 @@ class StartTest < Minitest::Test
 
   end
 
+  def test_new_turn_result_is_winners_deck_is_larger
+    card1 = Card.new(:club, '2', 2)
+    card2 = Card.new(:club, '3', 3)
+    card3 = Card.new(:club, '10', 10)
+    card4 = Card.new(:club, '9', 9)
+    cards1 = [card1, card2]
+    cards2 = [card3, card4]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Shark', deck1)
+    player2 = Player.new('Guppie', deck2)
+    turn = Turn.new(player1, player2)
+    start = Start.new(turn)
+    #binding.pry
+
+    #new_turn returns spoils of war
+    assert_equal [card1, card3], start.new_turn
+
+  end
+
+  def test_it_plays_war_someone_looses
+    card1 = Card.new(:club, '2', 2)
+    card2 = Card.new(:club, '3', 3)
+
+    cards1 = [card1]
+    cards2 = [card2, card2]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Shark', deck1)
+    player2 = Player.new('Guppie', deck2)
+    turn = Turn.new(player1, player2)
+    start = Start.new(turn)
+    start.play_war
+    #binding.pry
+
+    assert_equal true, start.turn.player1.has_lost?
+    assert_equal false, start.turn.player2.has_lost?
+
+  end
+
 
 end #final
